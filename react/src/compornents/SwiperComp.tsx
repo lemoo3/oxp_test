8c252c0a924b2f6be1158a31cecd38ac4f302976
OXPINTRA\Y.Watanuki <watanuki@opexpark.co.jp>
2022-02-23 20:24:32 +0900

    Swiperの追加

--- react/src/compornents/SwiperComp.tsx
+++ react/src/compornents/SwiperComp.tsx
@@ -1,22 +1,47 @@
 import React from 'react'
 
 // Import Swiper React components
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 
 export const SwiperComp:React.FC = () =>{
     return(
         <>
         <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
             onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>
+                <img src="/src/img/img1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/src/img/img2.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/src/img/img3.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/src/img/img1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="/src/img/img2.jpg" />
+            </SwiperSlide>
+
         </Swiper>
         </>
     )
